33bc333bfd50e53a871761be06aa7dd9741bc28e
Diego Luna <49794452+Diego-Luna@users.noreply.github.com>
2022-03-01 17:52:54 -0500

    add ex01, norminette ready

---
+++ ex01/ft_strncpy.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dluna-lo <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/01 13:59:29 by dluna-lo          #+#    #+#             */
+/*   Updated: 2022/03/01 15:22:57 by dluna-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	index;
+
+	index = 0;
+	while (dest[index] != '\0' && index != n)
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	return (dest);
+}
