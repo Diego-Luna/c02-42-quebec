3ea04744d2e8df12179656d86e689094280dcd59
Diego Luna <49794452+Diego-Luna@users.noreply.github.com>
2022-03-02 16:13:37 -0500

    ejercicio ex00, ex01m solucionados

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: dluna-lo <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 08:02:43 by dluna-lo          #+#    #+#             */
-/*   Updated: 2022/03/02 12:28:32 by dluna-lo         ###   ########.fr       */
+/*   Updated: 2022/03/02 15:00:25 by dluna-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@ char	*ft_strcpy(char *dest, char *src)
 	int	index;
 
 	index = 0;
-	while (dest[index] != '\0' && src[index] != '\0')
+	while (src[index] != '\0')
 	{
 		dest[index] = src[index];
 		index++;
 	}
+	dest[index] = '\0';
 	return (dest);
 }
--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: dluna-lo <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 13:59:29 by dluna-lo          #+#    #+#             */
-/*   Updated: 2022/03/02 12:31:14 by dluna-lo         ###   ########.fr       */
+/*   Updated: 2022/03/02 16:12:12 by dluna-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,14 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	unsigned int	index;
 
 	index = 0;
-	while (dest[index] != '\0' && index != n)
+	while (src[index] != '\0' && index < n)
 	{
 		dest[index] = src[index];
+		index++;
+	}
+	while (index < n)
+	{
+		dest[index] = '\0';
 		index++;
 	}
 	return (dest);
